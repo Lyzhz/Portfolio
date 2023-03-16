fc6f5d743d3f12bde9e99373fcaae62dcd97d9b6
Lyz <luisnunes371@gmail.com>
2023-03-16 19:42:36 -0300

    Fix

--- src/app/pages/instituicoes/instituicoes.component.ts
+++ src/app/pages/instituicoes/instituicoes.component.ts
@@ -1,10 +1,13 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-instituicoes',
   templateUrl: './instituicoes.component.html',
-  styleUrls: ['./instituicoes.component.css']
+  styleUrls: ['./instituicoes.component.css'],
 })
+// Button Animation
 export class InstituicoesComponent {
-
+  isHovered = false;
 }
