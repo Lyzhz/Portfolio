c8352ea0a3a4fd225ae43aa312273cb94c43270a
Lyz <luyzhz371@hotmail.com>
2023-07-13 19:15:14 -0300

    Fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProjAng';
+  lang = false;
+
+  constructor(public translate: TranslateService, private router: Router) {
+    translate.addLangs(['br', 'en']);
+    translate.setDefaultLang('en');
+    // const browserLang = translate.getBrowserLang();
+    // translate.use(browserLang.match(/pt|en/) ? browserLang : 'pt');
+   }
+
+    // ngOnInit() { 
+    //   let lang = false;
+    //   this.router.navigate([`/home/${lang}`]);
+    // }
+
+    switchLang() { 
+      const newlang = this.lang ? 'en' : 'br';
+      this.translate.use(newlang);
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// Importing Html Components
+
 import { InstituicoesComponent } from './pages/instituicoes/instituicoes.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutmeComponent } from './pages/aboutme/aboutme.component';
 import { CurriculumComponent } from './pages/curriculum/curriculum.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+
+// import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -21,9 +32,19 @@ import { ProjectsComponent } from './pages/projects/projects.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
